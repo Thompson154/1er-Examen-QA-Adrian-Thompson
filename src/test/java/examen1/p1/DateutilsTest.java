295d9b5ea81f5af9efbaf18fb2cd734c4a634841
Adrian Thompson <thompsonadrian210@gmail.com>
2024-10-07 17:41:24 -0400

    examen commit 1

---
+++ src/test/java/examen1/p1/DateutilsTest.java
@@ -0,0 +1,45 @@
+package examen1.p1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DateutilsTest {
+
+    //Probar con hoy que es Lunes
+    @Test
+    public void testDateutilsToday2024() {
+        DateUtils dateUtils = new DateUtils();
+        String expectedResult  = "MONDAY";
+        String actualResult = dateUtils.getDayOfWeek("2024-07-10");
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    //Probar con un año anterior
+    @Test
+    public void testDateutils2023() {
+        DateUtils dateUtils = new DateUtils();
+        String expectedResult  = "TUESDAY";
+        String actualResult = dateUtils.getDayOfWeek("2023-07-11");
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+
+    @Test
+    public void testDateutils2028() {
+        DateUtils dateUtils = new DateUtils();
+        String expectedResult  = "TUESDAY";
+        String actualResult = dateUtils.getDayOfWeek("2028-07-11");
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    //El metodo DateUtils solo funciona cada 5años bien delante o detras
+    @Test
+    public void testDateutils2033() {
+        DateUtils dateUtils = new DateUtils();
+        String expectedResult  = "TUESDAY";
+        String actualResult = dateUtils.getDayOfWeek("2028-07-11");
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+
+}
